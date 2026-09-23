87c7fe76621a9348c5a4a3c17f7a202a40343eb9
benji <benji@mail.briefkase.dev>
2026-09-23 19:05:30 +0200

    Configure app previews behind Shorucoto paths

--- bgstats/vite.config.ts
+++ bgstats/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/bgstats/",
+  preview: {
+    allowedHosts: ["b.kase-app.fr"],
+  },
   plugins: [react()],
   server: {
     proxy: {
--- micro/vite.config.ts
+++ micro/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/micro/",
+  preview: {
+    allowedHosts: ["b.kase-app.fr"],
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
